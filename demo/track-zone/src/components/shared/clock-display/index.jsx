5d378dbd137fb5357893b9f2cfc3c31df0e2c5c8
HM Nayem <hasan.m.nayem@gmail.com>
2022-06-22 23:15:53 +0600

    track-zone project - local clock display and action

---
+++ demo/track-zone/src/components/shared/clock-display/index.jsx
@@ -0,0 +1,18 @@
+import { format } from 'date-fns';
+const ClockDisplay = ({ date, title, timezone, offset }) => {
+	const offsetHr = offset / 60;
+	return (
+		<div>
+			<h1>Title: {title}</h1>
+			<h3>{format(date, "yyyy-MM-dd hh:mm:ss aaaaa'm'")}</h3>
+			<p>
+				{timezone}
+				{offsetHr > 0
+					? `+${Math.abs(offsetHr)}`
+					: `-${Math.abs(offsetHr)}`}
+			</p>
+		</div>
+	);
+};
+
+export default ClockDisplay;
--- demo/track-zone/src/hooks/useClock.jsx
+++ demo/track-zone/src/hooks/useClock.jsx
@@ -1,17 +1,6 @@
 import { useEffect, useState } from 'react';
 import { addMinutes } from 'date-fns';
 
-const init = {
-	id: '',
-	title: '',
-	timezone: {
-		type: '',
-		offset: '',
-	},
-	date_utc: null,
-	date: null,
-};
-
 const TIMEZONE_OFFSET = {
 	PST: -7 * 60,
 	EST: -4 * 60,
@@ -20,37 +9,40 @@ const TIMEZONE_OFFSET = {
 	MST: -6 * 60,
 };
 
-const useClock = (timezone, offset = 0) => {
-	const [state, setState] = useState({ ...init });
+const useClock = (timezone, offset) => {
+	const [localDate, setLocalDate] = useState(null);
+	const [localOffset, setLocalOffset] = useState(0);
+	const [localTimezone, setLocalTimezone] = useState('');
 	const [utc, setUTC] = useState(null);
 
 	useEffect(() => {
 		let d = new Date();
-		const localOffset = d.getTimezoneOffset();
-		d = addMinutes(d, localOffset);
+		const lo = d.getTimezoneOffset();
+		d = addMinutes(d, lo);
 		setUTC(d);
+		setLocalOffset(lo);
 	}, []);
 
 	useEffect(() => {
-		if (utc !== null && timezone) {
-			offset = TIMEZONE_OFFSET[timezone] ?? offset;
-			const newUtc = addMinutes(utc, offset);
-			setState({
-				...state,
-				date_utc: utc,
-				date: newUtc,
-			});
-		} else {
-			setState({
-				...state,
-				date_utc: utc,
-				date: utc,
-			});
+		if (utc !== null) {
+			if (timezone) {
+				offset = TIMEZONE_OFFSET[timezone] ?? offset;
+				const newUtc = addMinutes(utc, offset);
+				setLocalDate(newUtc);
+			} else {
+				const newUtc = addMinutes(utc, -localOffset);
+				const dateStrArr = newUtc.toUTCString().split(' ');
+				setLocalDate(newUtc);
+				setLocalTimezone(dateStrArr.pop());
+			}
 		}
-	}, [utc]);
+	}, [utc, timezone, offset]);
 
 	return {
-		clock: state,
+		date: localDate,
+		dateUTC: utc,
+		offset: offset || -localOffset,
+		timezone: timezone || localTimezone,
 	};
 };
 
