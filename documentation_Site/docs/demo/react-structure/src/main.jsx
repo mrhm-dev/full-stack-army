d8fa3d8039e5173ffa2ec78330df765029ba8397
Aldinn <kferdoush617@gmail.com>
2022-07-07 20:09:45 +0600

    first commit

--- documentation_Site/docs/demo/react-structure/src/main.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './app/App';
-import './main.css';
-
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
--- documentation_Site/docs/demo/react-structure/src/utils/object-utils.js
+++
@@ -1,7 +0,0 @@
-export const isObjEmpty = (obj) => {
-	return Object.keys(obj).length === 0;
-};
-
-export const deepClone = (obj) => {
-	return JSON.parse(JSON.stringify(obj));
-};
