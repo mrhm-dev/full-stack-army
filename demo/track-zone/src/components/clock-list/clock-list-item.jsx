c25bcd52335b23f063b55b1bd6bdb1e61922c510
HM Nayem <hasan.m.nayem@gmail.com>
2022-07-07 23:10:33 +0600

    Trac-zone project - create events functionality and useTimer hook

--- demo/track-zone/src/components/clock-list/clock-list-item.jsx
+++ demo/track-zone/src/components/clock-list/clock-list-item.jsx
@@ -1,22 +1,25 @@
-import { formatDistance } from 'date-fns';
+import { formatDistance, addSeconds } from 'date-fns';
+import { useState, useEffect } from 'react';
 import useClock from '../../hooks/useClock';
+import useTimer from '../../hooks/useTimer';
 import ClockActions from '../shared/clock-actions';
 import ClockDisplay from '../shared/clock-display';
 
 const ClockListItem = ({ clock, localClock, updateClock, deleteClock }) => {
 	const { date } = useClock(clock.timezone, clock.offset);
+	const timer = useTimer(date);
 
-	if (!date) return null;
+	if (!date || !timer) return null;
 
 	return (
 		<div>
 			<ClockDisplay
-				date={date}
+				date={timer}
 				title={clock.title}
 				timezone={clock.timezone}
 				offset={clock.offset}
 			/>
-			<h3>{formatDistance(localClock, date)}</h3>
+			<h3>{formatDistance(localClock, timer)}</h3>
 			<ClockActions
 				clock={clock}
 				updateClock={updateClock}
