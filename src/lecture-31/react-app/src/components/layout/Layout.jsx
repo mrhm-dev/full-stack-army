3d5c5907ee9e070e311bbbc251047214a6393fb0
Aditya Chakraborty <aditya09me@gmail.com>
2022-07-25 17:49:17 +0600

    lecture 31 overview in progress

---
+++ src/lecture-31/react-app/src/components/layout/Layout.jsx
@@ -0,0 +1,60 @@
+import { Link } from '@reach/router';
+import classes from './Layout.module.css';
+
+function Layout(props) {
+	const path = window.location.pathname;
+	return (
+		<div className={classes.app}>
+			<nav className={classes.nav}>
+				<div className={classes.brand}>
+					<h3>Brand Name</h3>
+				</div>
+				<div className={classes.menu}>
+					<ul>
+						<li>
+							<Link
+								to="/"
+								className={
+									path === '/' ? classes.navItemActive : classes.navItem
+								}
+							>
+								Home
+							</Link>
+						</li>
+						<li>
+							<Link
+								to="/about"
+								className={
+									path === '/about' ? classes.navItemActive : classes.navItem
+								}
+							>
+								About
+							</Link>
+						</li>
+						<li>
+							<Link
+								to="/help"
+								className={
+									path === '/help' ? classes.navItemActive : classes.navItem
+								}
+							>
+								Help
+							</Link>
+						</li>
+					</ul>
+				</div>
+			</nav>
+			<main className={classes.main}>{props.children}</main>
+			<footer className={classes.footer}>
+				<h3>Footer</h3>
+				<ul>
+					<li>Footer Link One</li>
+					<li>Footer Link Two</li>
+					<li>Footer Link Three</li>
+				</ul>
+			</footer>
+		</div>
+	);
+}
+
+export default Layout;
