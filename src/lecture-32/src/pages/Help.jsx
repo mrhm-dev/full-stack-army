8d60437da4468f8990d8d2f9d5b5413dfcdd969d
adityackr <aditya09me@gmail.com>
2022-08-01 21:55:45 +0600

    ongoing lecture 32

--- src/lecture-32/src/pages/Help.jsx
+++ src/lecture-32/src/pages/Help.jsx
@@ -1,9 +1,49 @@
+import { useState } from 'react';
 import Layout from '../components/layout/Layout';
 
 const Help = () => {
+	const [name, setName] = useState('');
+	// const [state, setState] = useState({});
+
+	// setTimeout(() => {
+	// 	setState({ name: 'HM Nayem' });
+	// }, 1 * 1000);
+
+	// console.log('Rendering');
+
+	const data = [
+		{
+			name: 'Abdullah Turky',
+			email: 'turky@test.com',
+		},
+		{
+			name: 'Arjun Roy',
+			email: 'arjun@test.com',
+		},
+		{
+			name: 'Fahim Faisal',
+			email: 'fahim@test.com',
+		},
+	];
+
 	return (
 		<Layout>
-			<h1>Hello, I am Help page</h1>
+			{/* {name && <h1>Hello {name}, I am Help page</h1>}
+			{!name && <h1>Hello Guest, I am Help page</h1>} */}
+
+			{name ? (
+				<h1>Hello {name}, I am Help page</h1>
+			) : (
+				<h1>Hello Guest, I am Help page</h1>
+			)}
+
+			<ul>
+				{data.map((item) => (
+					<li>
+						{item.name}, ({item.email})
+					</li>
+				))}
+			</ul>
 		</Layout>
 	);
 };
