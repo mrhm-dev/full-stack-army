69d833e8642f821c21138f4849886d36298bcd8a
adityackr <aditya09me@gmail.com>
2022-08-03 16:43:10 +0600

    lecture-33 code

---
+++ src/lecture-33/data-demo/src/App.jsx
@@ -0,0 +1,15 @@
+const socialIcons = [
+	{
+		id: '123',
+		icon: 'fa-facebook-f',
+		color: '#3a589d',
+		tooltip: 'share on facebook',
+		link: 'https://facebook.com',
+	},
+];
+
+const App = () => {
+	return <h1>App</h1>;
+};
+
+export default App;
