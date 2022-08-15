143add8a99a5370ca9ae7083d138fa506888b808
HM Nayem <hasan.m.nayem@gmail.com>
2022-08-15 22:55:48 +0600

    introducing react-hook-form

--- demo/redux-playground/src/App.jsx
+++ demo/redux-playground/src/App.jsx
@@ -1,21 +1,53 @@
-import { useSelector } from 'react-redux';
-import './App.css';
-import Count from './components/Count';
-import IncrementBtn from './components/IncrementBtn';
-import DecrementBtn from './components/DecrementBtn';
-import History from './components/History';
+import { Input } from '@mui/material';
+import { useForm, Controller } from 'react-hook-form';
 
-function App() {
+const App = () => {
+	const {
+		control,
+		handleSubmit,
+		formState: { errors, touchedFields },
+		reset,
+	} = useForm({
+		mode: 'onSubmit',
+		defaultValues: {
+			name: '',
+			email: '',
+			age: 0,
+		},
+	});
+
+	const onValid = (data, obj) => {
+		console.log(data);
+		reset();
+	};
+
+	const onInvalid = (data) => {
+		console.log('Invalid', data);
+	};
+
 	return (
-		<div className='App'>
-			<Count />
-			<div>
-				<IncrementBtn />
-				<DecrementBtn />
-			</div>
-			<History />
+		<div>
+			<h1>Hello World</h1>
+			<form onSubmit={handleSubmit(onValid, onInvalid)}>
+				<Controller
+					name='name'
+					control={control}
+					render={({ field }) => <Input {...field} />}
+				/>
+				<Controller
+					name='email'
+					control={control}
+					render={({ field }) => <Input {...field} />}
+				/>
+				<Controller
+					name='age'
+					control={control}
+					render={({ field }) => <Input {...field} />}
+				/>
+				<button type='submit'>Submit</button>
+			</form>
 		</div>
 	);
-}
+};
 
 export default App;
