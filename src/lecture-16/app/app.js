d7b1727e5b0a907d543509002ba802bfdef88eb1
Md. Maruf Sarker <78826405+mdmarufsarker@users.noreply.github.com>
2023-03-08 03:35:25 +0600

    Folders added by adding a README.md file inside it - Lecture-16

--- src/lecture-16/app/app.js
+++ src/lecture-16/app/app.js
@@ -1,14 +1,17 @@
 require('dotenv').config('../.env');
+
 const express = require('express');
 const { errorHandler, notFoundHandler } = require('./error');
-
+// const middleware = require('./middlewares/middleware');
 const app = express();
 
-app.use(require('./middleware'));
+// public static folder
+app.use(express.static('public'));
+
+// middlewares
+app.use(require('./middleware'))
 app.use(require('./routes'));
-
 app.use(notFoundHandler);
-
 app.use(errorHandler);
 
 module.exports = app;
--- src/lecture-16/server.js
+++ src/lecture-16/server.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
+
 const http = require('http');
 const app = require('./app/app');
 
 const server = http.createServer(app);
 
-const PORT = process.env.PORT || 8000;
-
+const PORT = process.env.PORT || 5000;
+// console.log(`PORT: ${PORT}`);
 server.listen(PORT, () => {
-	console.log(`Server is listening on PORT ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
+    console.log(`http://localhost:${PORT}`);
 });
